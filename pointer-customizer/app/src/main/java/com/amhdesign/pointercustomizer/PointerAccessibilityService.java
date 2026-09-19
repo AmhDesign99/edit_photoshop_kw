b3a37f1a0f23e5929db83d9d319363c1362b67fd
AmhDesign99 <maunuyul00@gmail.com>
2026-09-19 21:14:33 +0700

    Add Pointer Customizer Android v1.1

---
+++ pointer-customizer/app/src/main/java/com/amhdesign/pointercustomizer/PointerAccessibilityService.java
@@ -0,0 +1,9 @@
+package com.amhdesign.pointercustomizer;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class PointerAccessibilityService extends AccessibilityService {
+    @Override public void onAccessibilityEvent(AccessibilityEvent event) { }
+    @Override public void onInterrupt() { }
+}
